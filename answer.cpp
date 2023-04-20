88708860d6b810b780aef368b4b2572290b21e21
TaphouseGames <andreasfreiburg85@gmail.com>
2023-04-20 10:06:34 -0600

    change snake_case vars to camelCase for consistency

--- answer.cpp
+++ answer.cpp
@@ -5,18 +5,18 @@ using std::cout;
 using std::endl;
 
 
-Answer::Answer(const int point_penalty, const int point_reward) {
-    wrong_point_penalty = point_penalty;
-    correct_point_reward = point_reward;
+Answer::Answer(const int pointPenalty, const int pointReward) {
+    wrongPointPenalty = pointPenalty;
+    correctPointReward = pointReward;
 }
 
 void Answer::applyQuestionPenalty(Player &player) const {
-    cout << endl << "Losing " << wrong_point_penalty << " points for wrong answer" << endl;
-    player.points -= wrong_point_penalty;
+    cout << endl << "Losing " << wrongPointPenalty << " points for wrong answer" << endl;
+    player.points -= wrongPointPenalty;
 }
 
 void Answer::applyQuestionReward(Player &player) const {
-    cout << endl << "Gaining " << correct_point_reward << " points for correct answer" << endl;
-    player.points += correct_point_reward;
+    cout << endl << "Gaining " << correctPointReward << " points for correct answer" << endl;
+    player.points += correctPointReward;
 }
 
